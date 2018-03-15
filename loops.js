79ad2188e1a0b3ed1e8d1911f763bb060bc13d3e
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 19:38:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,5 +17,15 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  
+  do {
+  array.pop()
+  } while(array.lenght > 0 && maybeTrue())
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
